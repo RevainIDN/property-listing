dd60a5da8072195166d0de1fe7afdb916a8a9e8f
RevainIDN <kov.igor94@gmail.com>
2025-01-07 17:38:03 +0200

    Реализовано получение данных

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { PropertyData } from './types';
+import Navbar from './components/Navbar'
+import PropertyCard from './components/PropertyCard'
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [propertyData, setPropertyData] = useState<PropertyData[] | null>(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://raw.githubusercontent.com/devchallenges-io/web-project-ideas/main/front-end-projects/data/property-listing-data.json');
+      if (!response.ok) {
+        throw new Error("Error loading data");
+      }
+      const data = await response.json();
+      setPropertyData(data);
+    }
+    fetchData();
+  }, [])
 
   return (
     <>
-      <div>Some Text</div>
+      <img className='bg-img' src="bg.jpg" alt="bg" />
+      <div className='property-app'>
+        <div className='title-cont'>
+          <h1 className='title'>Peace, nature, dream</h1>
+          <p className='subtitle'>Find and book a great experience.</p>
+        </div>
+        <Navbar />
+        <h1 className='cards-title'>Over 200 stays</h1>
+        <ul className='card-list'>
+          {propertyData ? (
+            <PropertyCard
+              propertyData={propertyData}
+            />
+          ) : (
+            <p>Loading property cards...</p>
+          )}
+        </ul>
+      </div>
     </>
   )
 }
